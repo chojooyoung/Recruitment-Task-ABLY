59b07e6d80011670fcffea7f64db7b0deaaac6a9
jooyoung cho <jzo09@naver.com>
2022-03-21 16:32:19 +0900

    🔥 Remove: 사용하지 않는 store제거

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import { createStore } from "vuex";
 import Login from "./Login";
-import UserInfo from "./UserInfo";
 import ResetPassword from "./ResetPassword";
 
 export default createStore({
   modules: {
-    Login, UserInfo, ResetPassword,
+    Login, ResetPassword,
   },
 });
